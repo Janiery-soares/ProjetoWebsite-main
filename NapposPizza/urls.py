3e9f15ecaad5496cc1f5e095dde0d52d6b8eb383
mayke <mayke.salles.soares@gmail.com>
2023-10-18 20:40:49 -0300

    quase lá

--- NapposPizza/urls.py
+++ NapposPizza/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from apps.produtos.views import *
 from apps.cadastro.views import *
 from apps.vendas.views import *
@@ -13,12 +12,12 @@ urlpatterns = [
     # http://127.0.0.1:8000/admin/
     path('admin/', admin.site.urls),
 
-    path('cadastro/', CadastroView, include, name='pagina_cadastro'),
+    path('cadastro/', CadastroView, name='pagina_cadastro'),
 
-    path('produto/', ProdutosView, include, name='pagina_produto'),
+    path('produto/', ProdutosView, name='pagina_produto'),
 
-    path('vendas/', CompraView, include, name='pagina_vendas'),
+    path('vendas/', CompraView, name='pagina_vendas'),
 
-    path('core/', FornecedorView, include, name='pagina_fornecedor'),
+    path('core/', FornecedorView, name='pagina_fornecedor'),
 
 ]
