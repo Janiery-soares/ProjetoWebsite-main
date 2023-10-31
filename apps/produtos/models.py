c9c927fba4ceb84dd941245a244ee0f8f0a4812f
mayke <mayke.salles.soares@gmail.com>
2023-10-31 17:00:38 -0300

    jose

--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -24,7 +24,7 @@ class Pizza(models.Model):
     tamamho_pizza = models.CharField(max_length=8, choices=lista_tamanho, verbose_name='Tamanho da Pizza')
     descricao_pizza = models.TextField(verbose_name='Descrição da Pizza')
     quantidade_pizza = models.IntegerField(verbose_name='quantidade')
-    imagem_pizza = models.ImageField(upload_to="foto_pizza")
+    imagem_pizza = models.ImageField(upload_to="./midia/foto_pizza")
     preco = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
@@ -35,7 +35,7 @@ class Bebida(models.Model):
     nome_bebida = models.CharField(max_length=50)
     ml_bebida = models.IntegerField()
     quantidade_bebida = models.IntegerField()
-    imagem_refri = models.ImageField(upload_to="foto_refri")
+    imagem_refri = models.ImageField(upload_to="./midia/foto_refri")
     preco = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
