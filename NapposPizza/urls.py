9e60f9bbaf97fa19bc72ac2c4f7f517b9e59fbce
mayke <mayke.salles.soares@gmail.com>
2023-11-09 17:53:25 -0300

    site no ar

--- NapposPizza/urls.py
+++ NapposPizza/urls.py
@@ -4,6 +4,9 @@ from apps.produtos.views import *
 from apps.vendas.views import *
 from apps.core.views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 # http://127.0.0.1:8000/
 
@@ -12,7 +15,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include("apps.core.urls")),
-    path('', include("apps.produtos.urls")),
-
-   
+    path('', include("apps.produtos.urls")), 
 ]
+
+urlpatterns+= static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
